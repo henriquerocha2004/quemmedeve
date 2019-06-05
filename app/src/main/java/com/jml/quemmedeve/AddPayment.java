56b8eadf57b1b1cdb98590e1b3f8c03a56a1493c
Henrique Rocha <henriquerocha@MacBook-Air-de-Henrique.local>
2019-06-04 23:56:55 -0300

    Implementando o datepicker

--- app/src/main/java/com/jml/quemmedeve/AddPayment.java
+++ app/src/main/java/com/jml/quemmedeve/AddPayment.java
@@ -1,5 +1,6 @@
 package com.jml.quemmedeve;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.Calendar;
 import java.util.Locale;
 
 import ru.kolotnev.formattedittext.DecimalEditText;
@@ -28,6 +31,7 @@ public class AddPayment extends AppCompatActivity {
     private RadioGroup rdGrpFormPay;
     private DecimalEditText txtValueFull;
     private TextView txtDescPagamento;
+    private EditText datePaySplit;
     private Locale ptBR = new Locale("pt", "BR");
     private int valorParcelado;
 
@@ -39,10 +43,12 @@ public class AddPayment extends AppCompatActivity {
 
         txtValueFull = findViewById(R.id.txtValueFull);
         txtDescPagamento = findViewById(R.id.txtDescPagamento);
+        datePaySplit = findViewById(R.id.datePaySplit);
 
         mountSpinner();
         eventFormPayment();
         eventChangeValue();
+        datePicker();
     }
 
 
@@ -82,11 +88,13 @@ public class AddPayment extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 BigDecimal valorPagar = txtValueFull.getValue();
+                Object item = parent.getSelectedItem();
 
-                if(valorPagar == null){
+                if(valorPagar.toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Informe um valor para fazer o parcelamento!", Toast.LENGTH_LONG).show();
+                }else if(item.toString().isEmpty()){
+
                 }else{
-                    Object item = parent.getSelectedItem();
                     int parcela = Integer.parseInt(item.toString().substring(0, (item.toString().length() == 2 ? 1 : 2)));
                     BigDecimal valor = calcularParcelas(parcela, valorPagar);
                     String descPagamento = String.format("Parcelado em %s de R$ %,.2f", item.toString(), valor);
@@ -111,18 +119,15 @@ public class AddPayment extends AppCompatActivity {
 
                 String descPagamento = null;
 
-                System.out.println(spPaymentSplit.isEnabled());
-
                 if(spPaymentSplit.isEnabled() == true){
                     String itemSpinner = spPaymentSplit.getSelectedItem().toString();
-                    BigDecimal valorTotal = new BigDecimal(s.toString());
-                    String qtdParcelas = itemSpinner.substring(0, itemSpinner.indexOf("x") - 1);
+                    BigDecimal valorTotal = txtValueFull.getValue();
+                    String qtdParcelas = itemSpinner.substring(0, itemSpinner.indexOf("x"));
                     BigDecimal valorParcelado = calcularParcelas(Integer.parseInt(qtdParcelas), valorTotal);
                     descPagamento = String.format("Parcelado em %s de R$ %,.2f", qtdParcelas, valorParcelado);
 
                 }else {
-                    BigDecimal valorUnico = new BigDecimal(s.toString());
-                    descPagamento = String.format("Parcelado em 1x de R$ %,.2f", valorUnico);
+                    descPagamento = String.format("Dívida de R$ %,.2f", txtValueFull.getValue());
                 }
 
                 txtDescPagamento.setText(descPagamento);
@@ -134,6 +139,32 @@ public class AddPayment extends AppCompatActivity {
             }
         });
     }
+
+    private void datePicker(){
+
+        final Calendar c = Calendar.getInstance();
+
+        datePaySplit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                DatePickerDialog date = new DatePickerDialog(AddPayment.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int mYear, int mMonth, int mDayOfMonth) {
+                        datePaySplit.setText(mDayOfMonth + '/' + mMonth + '/' + mYear);
+                    }
+                }, day,month, year);
+
+                date.show();
+            }
+        });
+
+    }
+
 
 
     private BigDecimal calcularParcelas(int parcelas, BigDecimal valor){
